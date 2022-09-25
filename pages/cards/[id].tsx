19547f3f4ff4fe9c2bc8377da0d0f73751039260
Souhail Merroun <souhail.merroun1996@gmail.com>
2022-09-25 00:35:39 +0000

    Fix

--- pages/cards/[id].tsx
+++ pages/cards/[id].tsx
@@ -7,7 +7,7 @@ export async function getServerSideProps({
   query: { id: string };
 }) {
   const res = await fetch(
-    process.env.API_ENDPOINT_URL + `/items/cards/${id}`
+    process.env.NEXT_PUBLIC_API_ENDPOINT_URL + `/items/cards/${id}`
   );
   const { data }: CardData = await res.json();
   console.log("getServerSideProps");
